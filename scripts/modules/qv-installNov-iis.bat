66ba5eda97b6c8376a0b48b1d1e41f8ef1fbb00d
clint <clint.carr@outlook.com>
2018-02-13 10:05:53 +1100

    Fixes to QV IIS Scripts

---
+++ scripts/modules/qv-installNov-iis.bat
@@ -0,0 +1,1 @@
+START /WAIT "" c:\shared-content\binaries\QlikViewServer_x64Setup.exe /s /v" /qn IS_NET_API_LOGON_USERNAME="qmi-qv-iis\qservice" IS_NET_API_LOGON_PASSWORD="Qlik1234" ADDLOCAL="QVS,DistributionService,ManagementService,DirectoryServiceConnector,QvsClients,Plugin,AjaxZfc,MsIIS,QvTunnel,SupportTools,QvsDocs" DEFAULTWEBSITE="1" SILENT="1"" /clone_wait
