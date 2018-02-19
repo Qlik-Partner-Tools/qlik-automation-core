29c7993bd56a43cabaa6979fc8560bdadb20bd8f
clint <clint.carr@outlook.com>
2018-02-20 10:11:02 +1100

    Changed r repo

--- scripts/modules/r-install-packages.R
+++ scripts/modules/r-install-packages.R
@@ -1,2 +1,2 @@
 # install packages referenced by Qlik documentation
-install.packages(c("Rserve","rpart","d3r","partykit","jsonlite","ChainLadder","TTR","forecast","tseries","Matrix","colorspace"), dependencies = TRUE )
+install.packages(c("Rserve","rpart","d3r","partykit","jsonlite","ChainLadder","TTR","forecast","tseries","Matrix","colorspace"), repos = "http://cran.us.r-project.org", dependencies = TRUE )
