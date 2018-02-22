427bf53c671e856c3653d9d72da084441b76dec9
clint <clint.carr@outlook.com>
2018-02-22 15:08:09 +1100

    Changed q-user-setup to use Carbon

--- scripts/modules/q-user-setup.ps1
+++ scripts/modules/q-user-setup.ps1
@@ -3,9 +3,10 @@ Module:             q-user-setup
 Author:             Clint Carr
 Modified by:        -
 Modification History:
+ - Changed the creation of Qlik User to be based on Carbon
  - Added Logging
  - Added comments
-last updated:       10/09/2017
+last updated:       22/02/2018
 Intent: Disable Password complexity, create Qlik user and grant remote desktop rights
 #>
 
@@ -23,13 +24,13 @@ secedit /export /cfg c:\secpol.cfg  | Out-Null
 secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY | Out-Null
 rm -force c:\secpol.cfg -confirm:$false  | Out-Null
 
-### create Qlik User
-$password = ConvertTo-SecureString -String "Qlik1234" -AsPlainText -Force 
-New-LocalUser qlik -Password $password -FullName "Qlik User" -AccountNeverExpires -PasswordNeverExpires -ea Stop
-
 ### Install Carbon PowerShell Module
 Write-Log -Message "Installing carbon"
 cinst carbon -y | Out-Null
+
+### create Qlik User
+$password = ConvertTo-SecureString -String "Qlik1234" -AsPlainText -Force 
+Install-User -UserName Qlik -Password $password
 
 ### Grant Remote Admin Rights to Qlik User
 Write-Log -Message "Granting Qlik account Remote Interactive Logon Right"
