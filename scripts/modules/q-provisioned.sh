6e83f1d5bc0568151bad951b62aa26854eb4e9ba
Manuel Romero <mjromper@gmail.com>
2018-07-17 10:25:37 +0200

    Added q-provisioned for shell scripts

---
+++ scripts/modules/q-provisioned.sh
@@ -0,0 +1,14 @@
+# Execute any Shell scripts in ../Shared-Content/Scripts
+
+echo "Executing extra custom scripts in Shared-Content/Scripts."
+
+folder=/vagrant/shared-content/scripts
+
+if ([ -d "$folder" ] && [ "$(ls -A $folder)" ]); then
+    for entry in "$folder"/*
+    do
+        sh "$entry" -H || break 
+    done
+fi
+
+echo "Server provisioning Complete."
