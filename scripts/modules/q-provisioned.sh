2c77df59421456d272585ebfe32ac362aa9a139f
Manuel Romero <mjromper@gmail.com>
2019-07-29 12:50:25 +0200

    Update q-provisioned.sh

--- scripts/modules/q-provisioned.sh
+++ scripts/modules/q-provisioned.sh
@@ -1,16 +1,15 @@
-# Execute any Shell scripts in ../Shared-Content/Scripts
 
 folder=/vagrant/shared-content/scripts
 
 if ([ -d "$folder" ] && [ "$(ls -A $folder)" ]); then
     if ls -A $folder/*.sh 1> /dev/null 2>&1; then
-        echo "Executing extra custom scripts in Shared-Content/Scripts."
+        echo "Execute any Shell scripts in ../Shared-Content/Scripts"
         for entry in "$folder"/*.sh
         do
             sh "$entry" -H || break 
         done
     else
-        exit 1;
+        echo "There are no custom SHELL scripts."
     fi
 fi
 
