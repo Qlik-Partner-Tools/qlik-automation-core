095dd756fd7426565e24af578ad94d1b36474709
Leigh Kennedy <lkn@qlik.com>
2020-04-14 13:44:46 +1000

    missed

--- scripts/modules/qv-install-iis.ps1
+++ scripts/modules/qv-install-iis.ps1
@@ -1,7 +1,7 @@
 $qsVer = (Get-Content C:\shared-content\binaries\qver.json -raw) | ConvertFrom-Json
 
 write-log -Message "Installing $qsVer.name"
-if ( $qsVer.name -like "QlikView Server November 201*" -or $qsVer.name -like "QlikView*2019*") {
+if ( $qsVer.name -like "QlikView Server November 201*" -or $qsVer.name -like "QlikView*2019*"  -or $qsVer.name -like "QlikView*202*")  {
     write-log -Message "Install Mode: New"
     start-process -filepath c:\shared-content\scripts\modules\qv-installNov-iis.bat -wait
 }
