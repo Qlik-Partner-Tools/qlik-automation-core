495ee1088992e37458739c5f79248cde9d42a412
Leigh Kennedy <ljckennedy@gmail.com>
2021-05-13 16:01:37 +1000

    update vcredist ver

--- scripts/modules/q-net-framework-fx.ps1
+++ scripts/modules/q-net-framework-fx.ps1
@@ -1,5 +1,8 @@
 Write-log -Message "Installing .net Framework 4.8"
 cinst dotnetfx --no-progress
 
-Write-log -Message "Installing vcredist2017"
-cinst vcredist2017
+# Write-log -Message "Installing vcredist2017"
+# cinst vcredist2017
+
+Write-log -Message "Installing vcredist140"
+choco install vcredist140
